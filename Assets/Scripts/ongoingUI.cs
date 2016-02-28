840216182c3f0253ac520c2187e226cb6a961702
Pinia Chandra <ppnia@dhcp-128-189-223-85.ubcsecure.wireless.ubc.ca>
2016-02-28 10:04:30 -0800

    Add Cardboard stuffs

---
+++ Assets/Scripts/ongoingUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ongoingUI : MonoBehaviour {
+
+	Text textYourScore;
+	int currentScore;
+
+	void Start ()
+	{
+		textYourScore = GetComponent<Text> ();
+		currentScore = 0;
+		textYourScore.text = "Your Score: " + GetComponent<LevelManager>().GetScore ().ToString ();
+		//GUI.Label (textArea, "Current Score :" + currentScore.ToString());
+
+	}
+
+	void Update ()
+	{
+		
+
+		if ((GetComponent<LevelManager> ().GetScore ()) != currentScore) {
+			currentScore = GetComponent<LevelManager> ().GetScore ();
+			textYourScore.text = "Your Score: " + currentScore.ToString ();
+		}
+	}
+}
