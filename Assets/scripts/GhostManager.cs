b973356449656231026d418bc46729b9559ca0a2
Christopher Yang <chris.ct.yang@gmail.com>
2016-02-28 04:57:44 -0800

    Refactor GhostManager.cs

--- Assets/scripts/GhostManager.cs
+++ Assets/scripts/GhostManager.cs
@@ -1,40 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GhostManager : MonoBehaviour {
-
-	public GameObject ghost;
-	public GameObject player;
+public class GhostManager : MonoBehaviour
+{
 	public Vector3 playerPos;
 
-	// Use this for initialization
-	void Start () {
-		
-		for (int i = 0; i < 3; i++) {
-//			Instantiate(Resources.Load("Ghost"), new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
-
-			Vector3 pos = RandomCircle (playerPos, 10);
-			Quaternion rot = Quaternion.FromToRotation(Vector3.forward, playerPos-pos);
-			Instantiate (Resources.Load ("Ghost"), pos, rot);
+	void Start ()
+	{
+		playerPos = GameObject.FindWithTag ("Player").transform.position;
+		for (int i = 0; i < LevelManager.getGhostCount (); i++) {
+			Vector3 ghostPos = RandomCircle (playerPos, 10);
+			Quaternion ghostRot = Quaternion.FromToRotation (Vector3.forward, playerPos - ghostPos);
+			Instantiate (Resources.Load ("Ghost"), ghostPos, ghostRot);
 		}
 	}
 
-	Vector3 RandomCircle(Vector3 center, float radius) { 
-
-		// create random angle between 0 to 360 degrees 
-		float ang = Random.value * 360; 
-
+	Vector3 RandomCircle (Vector3 center, float radius)
+	{
+		float angle = Random.value * 360; 
 		Vector3 pos;
-
-		pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad); 
-		pos.y = center.y; // + radius * Mathf.Cos(ang * Mathf.Deg2Rad); 
-		pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad); 
+		pos.x = center.x + radius * Mathf.Sin (angle * Mathf.Deg2Rad); 
+		pos.y = center.y;
+		pos.z = center.z + radius * Mathf.Cos (angle * Mathf.Deg2Rad); 
 		return pos;
-	}
-
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 }
