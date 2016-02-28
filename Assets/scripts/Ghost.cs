17dec28443ec98af69e0e88afce0abad75e5aba1
Pinia Chandra <ppnia@dhcp-128-189-223-94.ubcsecure.wireless.ubc.ca>
2016-02-27 20:57:14 -0800

    Move scripts to Assets/scripts

--- Assets/scripts/Ghost.cs
+++ Assets/scripts/Ghost.cs
@@ -16,8 +16,11 @@ public class Ghost : MonoBehaviour {
 	//public int jumpScareRange = 1;
 	public bool staredAt = false;
 	public int farRange = 5;
+	public int farScore = 1;
 	public int mediumRange = 3;
+	public int mediumScore = 5;
 	public int closeRange = 1;
+	public int closeScore = 10;
 
 	void Start () {
 		//initialized x distance from camera(in level manager script);
@@ -43,33 +46,45 @@ public class Ghost : MonoBehaviour {
 			transform.position += transform.forward * movespeed * Time.deltaTime;
 		}
 
-		//within jump scare range? if yes, jump scare animation, and game over true.
-		//on collision
-		//make sure player has collider
-		//if (Vector3.Distance (transform.position, targetTransform.position) < jumpScareRange) {
-		//	JumpScare ();
-
-//		OnCollisionEnter ();
 
 	}
 
-//	void OnCollisionEnter (Collision col) {
-//		if (collider.gameObject.name == "Player") {
-//			JumpScare ();
-//		}
-//	}
+	//on trigger/collision, jump scare method
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "Player") {
+			//JumpScare ();
+			Debug.Log("bye");
+		}
+	}
 
-//	void Death () {
-//		//update level manager score
-//
-//		if ((transform.position
-//			levelManager.
-//			Destroy (gameObject);
-//			}
-//
-//			void JumpScare() {
-//				* //ghost comes in front of you, music plays
-//				GameOver (); -->SceneManager.LoadScene() move to level manager?
-//				*/
-//			}
+	void Death () {
+			//update level manager score
+
+	if (transform.position-target.position) > farRange) {
+		Destroy (GameObject);
+		}
+
+	if ((transform.position-target.position) < farRange) && ((transform.position) > mediumRange) {
+		levelManager.AddScore(farScore);
+		Destroy (GameObject);
+		}
+
+	if ((transform.position-target.position) > farRange) {
+		levelManager.AddScore(mediumScore);
+		Destroy (GameObject);
+		}
+
+	if ((transform.position-target.position) > farRange) {
+		levelManager.AddScore(closeScore);
+		Destroy (GameObject);
+		}
+	}
+		
+	void JumpScare() {
+		//ghost comes in front of you, music plays
+		target.FaceThisGhost(transform.position);
+		//GameOver (); -->SceneManager.LoadScene() move to level manager?
+		levelManager.ItsGameOver();
+
+		}
 }
---
+++ Assets/scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level : MonoBehaviour {
+
+	public int score;
+	public bool gameover = false;
+
+	// Use this for initialization
+	void Start () {
+		//initialize random generate ghost
+		//initialize score
+		//initialize level
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//if score is howmany, increase level
+		//generate more ghosts according to the level
+		//if gameover, change scene
+
+	}
+
+	void AddScore (int x) {
+		score += x;
+	}
+
+	void ItsGameOver () {
+		gameover = true;
+	}
+}
