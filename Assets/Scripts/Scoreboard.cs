1164b904c86ea6c038b7e328aabed7e0e48964b9
Christopher Yang <chris.ct.yang@gmail.com>
2016-02-28 06:04:02 -0800

    Create finish scene

---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scoreboard : MonoBehaviour
+{
+	Text textYourScore;
+
+	void Start ()
+	{
+		textYourScore = GetComponent<Text> ();
+		textYourScore.text = "Your Score: " + (LevelManager.GetScore ().ToString ());
+	}
+
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.Return)) {
+			SceneManager.LoadScene ("Main");
+		}
+	}
+}
