5fa5157a58b1bb9ce820a2491695e89b17f983fd
Pinia Chandra <ppnia@dhcp-128-189-223-94.ubcsecure.wireless.ubc.ca>
2016-02-27 19:13:37 -0800

    Add scripts for Ghost and Level

---
+++ Assets/Ghost.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Ghost : MonoBehaviour {
+
+	// Use this for initialization
+
+	//var target : Transform; 
+
+	public GameObject target;
+	public Transform targetTransform;
+	public GameObject levelManager;
+	public int movespeed = 2;
+	public int rotationspeed = 2;
+	//public int jumpScareRange = 1;
+	public bool staredAt = false;
+	public int farRange = 5;
+	public int mediumRange = 3;
+	public int closeRange = 1;
+
+	void Start () {
+		//initialized x distance from camera(in level manager script);
+		//detect main camera (player), update direction.
+
+		target = GameObject.FindWithTag ("Player");
+		targetTransform = target.transform;
+		levelManager = GameObject.FindWithTag ("LevelManager");
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		//rotate towards player
+		transform.rotation = Quaternion.Slerp (transform,
+			Quaternion.LookRotation (targetTransform.position - transform.position),
+			rotationspeed * Time.deltaTime);
+
+		//walk forward towards player
+		while (!staredAt) {
+			transform.position += transform.forward * movespeed * Time.deltaTime;
+		}
+
+		//within jump scare range? if yes, jump scare animation, and game over true.
+		//on collision
+		//make sure player has collider
+		//if (Vector3.Distance (transform.position, targetTransform.position) < jumpScareRange) {
+		//	JumpScare ();
+
+		OnCollisionEnter ();
+
+	}
+
+	void OnCollisionEnter (Collision col) {
+		if (collider.gameObject.name == "Player") {
+			JumpScare ();
+		}
+	}
+
+	void Death () {
+		//update level manager score
+
+		if ((transform.position
+			levelManager.
+			Destroy (gameObject);
+			}
+
+			void JumpScare() {
+				* //ghost comes in front of you, music plays
+				GameOver (); -->SceneManager.LoadScene() move to level manager?
+				*/
+			}
---
+++ Assets/Level.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	//initialize random generate ghost
+	//initialize score
+	//initialize level
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	//if score is howmany, increase level
+	//generate more ghosts according to the level
+	//if gameover, change scene
+	//ask player if theres score+?
+
+	}
+}
