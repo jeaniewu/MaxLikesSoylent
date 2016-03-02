6518dcb537fc4d4b55ae306cfb98a60d8f2069f0
Christopher Yang <chris.ct.yang@gmail.com>
2016-03-02 01:24:45 -0800

    Prevent BGM from looping

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,16 +2,29 @@
 using System.Collections;
 
 public class SoundManager : MonoBehaviour {
+
+	private static SoundManager instance = null;
+
+	public static SoundManager Instance {
+		get { return Instance; }
+	}
+
 	void Awake () {
+		if (instance != null && instance != this) {
+			Destroy (this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
 		DontDestroyOnLoad (gameObject);
 	}
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
